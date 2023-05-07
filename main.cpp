28fb23ce0fab186b19fd31647f670248177e1f77
Kupiwa Nangati <70841052+Kupiwa@users.noreply.github.com>
2023-05-07 17:15:15 +0200

    Update main.cpp
    modified main file

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@
 #include <time.h>
 #include "board.h"
 #include "agent.h"
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -33,149 +36,316 @@ int main(int argc, char** argv) {
     cout << "--------------------------------------------" << endl;
 
     //Options for the game
-    cout << "Enter a number between(1-3): ";
-    cout << "1 - New Game";
-    cout << "2 - Replay";
-    cout << "3 - Resume";
+    cout << "Enter a number between(1-3): "<<endl;
+    cout << "1 - New Game"<<endl;
+    cout << "2 - Replay"<<endl;
+    cout << "3 - Resume"<<endl;
     
     int num = 0; // Variable for Option chosen
-  
+    //ofstream storeSeed("storeSeed.txt");
+
+    //ofstream lastMove("moves.txt");
+
+
     cin >> num; //  Option chosen
     int diff =0; // Variable for Difficulty chosen
     if (num == 1){
-        //cout << "Enter difficulty level (1: easy/2: hard): ";  
-       // cin >> diff ; // Difficulty chosen
+        cout << "Enter difficulty level (1: easy/2: hard): ";  
+        cin >> diff ; // Difficulty chosen
 
-       // if (diff == 1){
+        if (diff == 1){
 
-        time_t now = time(NULL);
-        srand(now);
-        cout<<"Seed: "<<now<<endl;
-        board abalone;
-        string state(abalone);
-        cout<<"Initial State:"<<endl<<state;
-        /*agent* w = new randAgent('O');
-        agent* b = new distAgent('@');
-        movement* pom=nullptr;//previous opponent's move
-        movement* mym=nullptr;//player's chosen move
-        char c='O';
-        for(int i=0;i<200;i++){
-            try{
-                if(c=='O'){
-                    movement mm = w->move(pom,abalone);
-                    mym = new movement(mm);
-                    if(pom)
-                        delete pom;
-                    pom = new movement(mm);
+            time_t now = time(NULL); // store the now object into a file.
+            srand(now); // replay several games
+
+            // store the now object into a file and then 
+            ofstream storeSeed("storeSeed.txt");
+            storeSeed<<now<<endl;
+            storeSeed.close();
+
+
+            cout<<"Seed: "<<now<<endl;
+            board abalone;
+            string state(abalone);
+            cout<<"Initial State:"<<endl<<state;
+            agent* w = new randAgent('O');
+            agent* b = new distAgent('@');
+            movement* pom=nullptr;//previous opponent's move
+            movement* mym=nullptr;//player's chosen move
+            char c='O';
+            for(int i=0;i<200;i++){
+                try{
+                    if(c=='O'){
+                        movement mm = w->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                    else{
+                        movement mm = b->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                }
+                catch(const string& s){
+                    cout<<s;
+                    return 1;
+                }        
+                bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
+                cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
+                if(valid){
+                    string state(abalone);         
+                    cout<<"Next State:"<<endl<<state; 
                 }
                 else{
-                    movement mm = b->move(pom,abalone);
-                    mym = new movement(mm);
-                    if(pom)
-                        delete pom;
-                    pom = new movement(mm);
+                    cout<<"Invalid move!!"<<endl;
                 }
+                if(c=='O') 
+                    c='@';
+                else
+                    c='O'; 
+                delete mym;
+                SLP(1);
+                //system(CL);
+
+                ofstream lastMove("moves.txt");
+                lastMove<<i;
+                lastMove.close();
             }
-            catch(const string& s){
-                cout<<s;
-                return 1;
-            }        
-            bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
-            cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
-            if(valid){
-                string state(abalone);         
-                cout<<"Next State:"<<endl<<state; 
-            }
-            else{
-                cout<<"Invalid move!!"<<endl;
-            }
-            if(c=='O') 
-                c='@';
-            else
-                c='O'; 
-            delete mym;
-            SLP(1);
-            //system(CL);
+            if(pom)
+                delete pom;
+            delete w;
+            delete b;
+            
+            return 0;           
         }
-        if(pom)
-            delete pom;
-        delete w;
-        delete b;
-        
-        return 0;           
+
+        else if (diff == 2){
+            time_t now = time(NULL);
+            srand(now);
+            cout<<"Seed: "<<now<<endl;
+            board abalone;
+            string state(abalone);
+            cout<<"Initial State:"<<endl<<state;
+            agent* w = new distAgent('O');
+            agent* b = new distAgent('@');
+            movement* pom=nullptr;//previous opponent's move
+            movement* mym=nullptr;//player's chosen move
+            char c='O';
+            for(int i=0;i<200;i++){
+                try{
+                    if(c=='O'){
+                        movement mm = w->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                    else{
+                        movement mm = b->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                }
+                catch(const string& s){
+                    cout<<s;
+                    return 1;
+                }        
+                bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
+                cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
+                if(valid){
+                    string state(abalone);         
+                    cout<<"Next State:"<<endl<<state; 
+                }
+                else{
+                    cout<<"Invalid move!!"<<endl;
+                }
+                if(c=='O') 
+                    c='@';
+                else
+                    c='O'; 
+                delete mym;
+                SLP(1);
+                //system(CL);
+                ofstream lastMove("moves.txt");
+                lastMove<<i;
+                lastMove.close();
+            }
+            if(pom)
+                delete pom;
+            delete w;
+            delete b;
+            
+            return 0;           
+        }
+
+    
+
     }
+    else if (num == 2 ){
+            //Munazzah & Kupiwa & Ntando
+            //time_t now = time(NULL); // store the now object into a file.
 
-    else if (diff == 2){
-         time_t now = time(NULL);
-        srand(now);
-        cout<<"Seed: "<<now<<endl;
-        board abalone;
-        string state(abalone);
-        cout<<"Initial State:"<<endl<<state;
-        agent* w = new distAgent('O');
-        agent* b = new distAgent('@');
-        movement* pom=nullptr;//previous opponent's move
-        movement* mym=nullptr;//player's chosen move
-        char c='O';
-        for(int i=0;i<200;i++){
-            try{
-                if(c=='O'){
-                    movement mm = w->move(pom,abalone);
-                    mym = new movement(mm);
-                    if(pom)
-                        delete pom;
-                    pom = new movement(mm);
+            ifstream storeSeed("storeSeed.txt");
+            int now;
+            string line;
+            getline(storeSeed, line);
+            //now = stoul(line);
+            stringstream ss(line);
+            ss>>now;
+            srand(now); // replay several games
+
+            //ofstream lastMove("moves.txt");
+
+            board abalone;
+            string state(abalone);
+            cout<<"Initial State:"<<endl<<state;
+            agent* w = new randAgent('O');
+            agent* b = new distAgent('@');
+            movement* pom=nullptr;//previous opponent's move
+            movement* mym=nullptr;//player's chosen move
+            char c='O';
+            int n = 5;
+            for(int i=0;i<200;i++){
+                try{
+                    if(c=='O'){
+                        movement mm = w->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                    else{
+                        movement mm = b->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                }
+                catch(const string& s){
+                    cout<<s;
+                    return 1;
+                }        
+                bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
+                cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
+                if(valid){
+                    string state(abalone);         
+                    cout<<"Next State:"<<endl<<state; 
                 }
                 else{
-                    movement mm = b->move(pom,abalone);
-                    mym = new movement(mm);
-                    if(pom)
-                        delete pom;
-                    pom = new movement(mm);
+                    cout<<"Invalid move!!"<<endl;
                 }
+                if(c=='O') 
+                    c='@';
+                else
+                    c='O'; 
+                delete mym;
+                SLP(1);
+                //system(CL);
+                ofstream lastMove("moves.txt");
+                lastMove<<i;
+                lastMove.close();
             }
-            catch(const string& s){
-                cout<<s;
-                return 1;
-            }        
-            bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
-            cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
-            if(valid){
-                string state(abalone);         
-                cout<<"Next State:"<<endl<<state; 
-            }
-            else{
-                cout<<"Invalid move!!"<<endl;
-            }
-            if(c=='O') 
-                c='@';
-            else
-                c='O'; 
-            delete mym;
-            SLP(1);
-            //system(CL);
-        }
-        if(pom)
-            delete pom;
-        delete w;
-        delete b;
-        
-        return 0;           
+            if(pom)
+                delete pom;
+            delete w;
+            delete b;
+
+            storeSeed.close();
+            
+            return 0;      
+         
     }
     
 
-    else if (num == 2 ){
-        // Munazzah & Kupiwa 
-    }
-
     else if (num == 3){
-        //Ntando & Kupiwa
+        //Ntando & Kupiwa & Munazzah
+            ifstream storeSeed("storeSeed.txt");
+            int now;
+            string line;
+            getline(storeSeed, line);
+            stringstream ss(line);
+            ss>>now;
+            srand(now); // replay several games
+
+            board abalone;
+            string state(abalone);
+            
+            agent* w = new randAgent('O');
+            agent* b = new distAgent('@');
+            movement* pom=nullptr;//previous opponent's move
+            movement* mym=nullptr;//player's chosen move
+            char c='O';
+            int n = 5;
+            for(int i=0;i<200;i++){
+                try{
+                    if(c=='O'){
+                        movement mm = w->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                    else{
+                        movement mm = b->move(pom,abalone);
+                        mym = new movement(mm);
+                        if(pom)
+                            delete pom;
+                        pom = new movement(mm);
+                    }
+                }
+                catch(const string& s){
+                    cout<<s;
+                    return 1;
+                }        
+                bool valid=abalone.executeMove(c,mym->l,mym->n,mym->fd,mym->md);
+
+                ifstream lastM("moves.txt");
+                int lastMoveReadFromFile;
+                string line;
+                while(getline(lastM, line)) {
+                    stringstream ss(line);
+                    ss>>lastMoveReadFromFile;
+                }
+             
+                if(i > lastMoveReadFromFile) {
+                    cout<<"Move "<<i+1<<": "<<c<<","<<mym->l<<","<<mym->n<<","<<mym->fd<<","<<mym->md<<endl;
+                    if(valid){
+                        string state(abalone);         
+                        cout<<"Next State:"<<endl<<state; 
+                    }
+                    else{
+                        cout<<"Invalid move!!"<<endl;
+                    }
+                }
+                if(c=='O') 
+                    c='@';
+                else
+                    c='O'; 
+                delete mym;
+                SLP(1);
+                //system(CL);
+            }
+            if(pom)
+                delete pom;
+            delete w;
+            delete b;
+
+            storeSeed.close();
+            
+            return 0;      
     }
 
     else{
         cout << "Enter a number between 1 and 3" << endl;
     }
-*/
+
 
     /*
     
@@ -193,7 +363,9 @@ int main(int argc, char** argv) {
 
 
 
-}}
+}
+
+
 
 
   
